e254ae684e8a64684f5e9635681dfa8da33cca11
Martin Keogh <martin1keogh@gmail.com>
2013-06-30 22:01:58 +0200

    IrcReader Initial commit

--- src/main/scala/IRCBot/IrcReader.scala
+++ src/main/scala/IRCBot/IrcReader.scala
@@ -1,12 +1,58 @@
 package IRCBot
 
-/**
- * Created with IntelliJ IDEA.
- * User: martin
- * Date: 30/06/13
- * Time: 00:50
- * To change this template use File | Settings | File Templates.
- */
-class IrcReader {
+import UI.Reader
+import GameLogic.{Partie, Card}
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+
+class IrcReader extends Reader {
+
+  /*
+  Really need to use actors here
+   */
+
+  class State(var modifiable:Boolean,var modified:Boolean, var couleur:String, var contrat:Int)
+
+  val enchere = new State(false,false,"",0)
+  def getCouleur: Int = {
+    // Re-initialisation
+    enchere.couleur = ""
+    enchere.contrat = 0
+    enchere.modified = false
+    enchere.modifiable = true
+    while (!enchere.modified) {Thread.sleep(1000)}
+    enchere.modifiable = false
+    enchere.couleur.toUpperCase match {
+      case "PIQUE" | "P" => 1
+      case "CARREAU" | "CA" => 2
+      case "TREFLE" | "T"=> 3
+      case "COEUR" | "CO"=> 4
+      case "TA" => 5
+      case "SA" => 6
+      case "PASSE" => 0
+      case _ => getCouleur
+    }
+  }
+
+  // 'contrat' is set during getCouleur
+  def getContrat: Int = {
+    enchere.contrat
+  }
+
+  // Are we waiting for someone to play a card ?
+  // which card is it ?
+  var (modifiable,card) = (false,"")
+  def getCard(jouables: List[Card], autres: List[Card]): Card = {
+    card = ""
+    modifiable = true
+    println(modifiable)
+    while (card.isEmpty) Thread.sleep(1000)
+    modifiable = false
+    try {jouables(card.toInt)}
+    catch {
+      case e:NumberFormatException => getCard(jouables,autres)
+      case e:IndexOutOfBoundsException => getCard(jouables,autres)
+    }
+  }
 
 }
